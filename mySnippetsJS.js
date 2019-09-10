9a7dde146157c82ffaf9ffc41496bd9d8f0a69a7
avilaacejohn <avilaacejohn@gmail.com>
2019-09-10 09:33:01 +0800

    Update JS file

---
+++ mySnippetsJS.js
@@ -0,0 +1,24 @@
+/**
+ ** Inserting element inside DOM
+ **/
+var newclients = $('content here');
+newclients.insertAfter('.class_or_id');
+
+/**
+ ** Hide and show modal
+ **/
+
+$('#modal').modal('hide');
+$('#modal').modal('show');
+
+/**
+ ** Empty input field
+ **/
+
+ $('#yourid').val('');
+
+ /**
+ ** Add CSS style
+ **/
+
+ $('#elementID').css('display', 'none');
