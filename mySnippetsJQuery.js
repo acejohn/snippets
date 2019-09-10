d9483c6e13b3275c1a9f52871eb07fadc49c162a
avilaacejohn <avilaacejohn@gmail.com>
2019-09-10 09:58:54 +0800

    removed jquery code from js and moved to mySnippetsJquery

---
+++ mySnippetsJQuery.js
@@ -0,0 +1,24 @@
+/**
+ ** Hide and show modal
+ **/
+
+$('#modal').modal('hide');
+$('#modal').modal('show');
+
+/**
+ ** Empty input field
+ **/
+
+ $('#yourid').val('');
+
+ /**
+ ** Add CSS style
+ **/
+
+ $('#elementID').css('display', 'none');
+
+ /**
+ ** Disabled button
+ **/
+
+ $('#btnID').attr('disabled', true);
--- mySnippetsJS.js
+++ mySnippetsJS.js
@@ -3,22 +3,3 @@
  **/
 var newclients = $('content here');
 newclients.insertAfter('.class_or_id');
-
-/**
- ** Hide and show modal
- **/
-
-$('#modal').modal('hide');
-$('#modal').modal('show');
-
-/**
- ** Empty input field
- **/
-
- $('#yourid').val('');
-
- /**
- ** Add CSS style
- **/
-
- $('#elementID').css('display', 'none');
